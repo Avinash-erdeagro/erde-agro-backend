10ff02944fdee57dae28e9e2565ed04c7ce1244c
Avinash Mohare <mohareavinash80@gmail.com>
2026-04-07 14:09:26 +0530

    added models to admin

---
+++ farmerapp/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from .models import SoilType, IrrigationType, CropType, Farm, FarmCrop
+
+
+@admin.register(SoilType)
+class SoilTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "irriwatch_id")
+
+
+@admin.register(IrrigationType)
+class IrrigationTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "irriwatch_id")
+
+
+@admin.register(CropType)
+class CropTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "irriwatch_id")
+
+
+class FarmCropInline(admin.TabularInline):
+    model = FarmCrop
+    extra = 0
+
+
+@admin.register(Farm)
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ("id", "land_record_number", "farmer", "soil_type", "irrigation_type")
+    inlines = [FarmCropInline]
+
+
+@admin.register(FarmCrop)
+class FarmCropAdmin(admin.ModelAdmin):
+    list_display = ("id", "farm", "crop_type", "plantation_date", "is_active")
