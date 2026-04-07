3879a326cd71ed38281e3277f593834005b99c1a
Avinash Mohare <mohareavinash80@gmail.com>
2026-04-07 14:53:20 +0530

    Remove irriwatch_id from serializer fields and enforce authentication for lookup viewsets

--- farmerapp/api/serializers/lookups.py
+++ farmerapp/api/serializers/lookups.py
@@ -5,16 +5,16 @@ from farmerapp.models import SoilType, IrrigationType, CropType
 class SoilTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = SoilType
-        fields = ["id", "name", "irriwatch_id"]
+        fields = ["id", "name"]
 
 
 class IrrigationTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = IrrigationType
-        fields = ["id", "name", "irriwatch_id"]
+        fields = ["id", "name"]
 
 
 class CropTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CropType
-        fields = ["id", "name", "irriwatch_id"]
+        fields = ["id", "name"]
--- farmerapp/api/views/lookups.py
+++ farmerapp/api/views/lookups.py
@@ -2,18 +2,22 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from authapp.api.views.base import FormattedResponseMixin
 from farmerapp.models import SoilType, IrrigationType, CropType
 from farmerapp.api.serializers import SoilTypeSerializer, IrrigationTypeSerializer, CropTypeSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class SoilTypeViewSet(FormattedResponseMixin, ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = SoilType.objects.all()
     serializer_class = SoilTypeSerializer
 
 
 class IrrigationTypeViewSet(FormattedResponseMixin, ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = IrrigationType.objects.all()
     serializer_class = IrrigationTypeSerializer
 
 
 class CropTypeViewSet(FormattedResponseMixin, ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = CropType.objects.all()
     serializer_class = CropTypeSerializer
